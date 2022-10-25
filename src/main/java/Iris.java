0065f9eb36b36db7f11fabcb14e7509d853b373f
Thomas Dujardin <thomas.dujardin2.etu@univ-lille.fr>
2022-10-25 11:51:21 +0200

    convention

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Iris implements Point {
 
     @Override
@@ -9,4 +11,55 @@ public class Iris implements Point {
     public double getNormalizedValue(Column<? extends Point> xColumn) {
         return 0;
     }
+    
+
+	@CsvBindByName(column = "sepal.length") 
+	public double sepalLength;
+	@CsvBindByName(column = "sepal.width")
+	public double sepalWidth;
+	@CsvBindByName(column = "petal.length")
+	public double petalLength;
+	@CsvBindByName(column = "petal.width")
+	public double petalWidth;
+	@CsvBindByName(column = "variety")
+	public String variety;
+	
+	public Iris() {
+	}
+	
+	
+
+
+	public double getSepal_length() {
+		return sepalLength;
+	}
+
+	public double getSepal_width() {
+		return sepalWidth;
+	}
+
+	public double getPetal_length() {
+		return petalLength;
+	}
+
+	public double getPetal_width() {
+		return petalWidth;
+	}
+
+	public String getVariety() {
+		return variety;
+	}
+
+	public List<Iris> getClosest(List<Iris> elements, int number) {
+	    // TODO Auto-generated method stub
+	    return null;
+	}
+
+	@Override
+	public String toString() {
+		return "Iris [sepalLength=" + sepalLength + ", sepalWidth=" + sepalWidth + ", petalLength=" + petalLength
+				+ ", petalWidth=" + petalWidth + ", variety=" + variety + "]";
+	}
+
+	
 }
