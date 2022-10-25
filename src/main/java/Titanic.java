e6fa40967488cd320464ea75a3e09b988ab1b8be
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-10-25 11:25:48 +0200

    Add Iris and Titanic classes
    Add two classes that implement Point interface

---
+++ src/main/java/Titanic.java
@@ -0,0 +1,12 @@
+public class Titanic implements Point {
+
+    @Override
+    public Object getValue(Column<? extends Point> column) {
+        return null;
+    }
+
+    @Override
+    public double getNormalizedValue(Column<? extends Point> xColumn) {
+        return 0;
+    }
+}
