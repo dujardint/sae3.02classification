9f01a554eb69a232d358d531f6f98c09b7055459
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-10-25 13:04:09 +0200

    Fix imports + Create builder for Iris
    - Add setters to allow builder to init class fields
    - Rename getters according to conventions

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,6 +1,66 @@
+import com.opencsv.bean.CsvBindByName;
+
 import java.util.List;
 
 public class Iris implements Point {
+
+    @CsvBindByName(column = "sepal.length")
+    public double sepalLength;
+    @CsvBindByName(column = "sepal.width")
+    public double sepalWidth;
+    @CsvBindByName(column = "petal.length")
+    public double petalLength;
+    @CsvBindByName(column = "petal.width")
+    public double petalWidth;
+    @CsvBindByName(column = "variety")
+    public String variety;
+
+    protected Iris() {
+    }
+
+    public double getSepalLength() {
+        return sepalLength;
+    }
+
+    public void setSepalLength(double sepalLength) {
+        this.sepalLength = sepalLength;
+    }
+
+    public double getSepalWidth() {
+        return sepalWidth;
+    }
+
+    public void setSepalWidth(double sepalWidth) {
+        this.sepalWidth = sepalWidth;
+    }
+
+    public double getPetalLength() {
+        return petalLength;
+    }
+
+    public void setPetalLength(double petalLength) {
+        this.petalLength = petalLength;
+    }
+
+    public double getPetalWidth() {
+        return petalWidth;
+    }
+
+    public void setPetalWidth(double petalWidth) {
+        this.petalWidth = petalWidth;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    public List<Iris> getClosest(List<Iris> elements, int number) {
+        return null;
+    }
 
     @Override
     public Object getValue(Column<? extends Point> column) {
@@ -11,55 +71,10 @@ public class Iris implements Point {
     public double getNormalizedValue(Column<? extends Point> xColumn) {
         return 0;
     }
-    
 
-	@CsvBindByName(column = "sepal.length") 
-	public double sepalLength;
-	@CsvBindByName(column = "sepal.width")
-	public double sepalWidth;
-	@CsvBindByName(column = "petal.length")
-	public double petalLength;
-	@CsvBindByName(column = "petal.width")
-	public double petalWidth;
-	@CsvBindByName(column = "variety")
-	public String variety;
-	
-	public Iris() {
-	}
-	
-	
-
-
-	public double getSepal_length() {
-		return sepalLength;
-	}
-
-	public double getSepal_width() {
-		return sepalWidth;
-	}
-
-	public double getPetal_length() {
-		return petalLength;
-	}
-
-	public double getPetal_width() {
-		return petalWidth;
-	}
-
-	public String getVariety() {
-		return variety;
-	}
-
-	public List<Iris> getClosest(List<Iris> elements, int number) {
-	    // TODO Auto-generated method stub
-	    return null;
-	}
-
-	@Override
-	public String toString() {
-		return "Iris [sepalLength=" + sepalLength + ", sepalWidth=" + sepalWidth + ", petalLength=" + petalLength
-				+ ", petalWidth=" + petalWidth + ", variety=" + variety + "]";
-	}
-
-	
+    @Override
+    public String toString() {
+        return "Iris [sepalLength=" + sepalLength + ", sepalWidth=" + sepalWidth + ", petalLength=" + petalLength
+                + ", petalWidth=" + petalWidth + ", variety=" + variety + "]";
+    }
 }
---
+++ src/main/java/IrisBuilder.java
@@ -0,0 +1,43 @@
+public class IrisBuilder {
+
+    public double sepalLength;
+    public double sepalWidth;
+    public double petalLength;
+    public double petalWidth;
+    public String variety;
+
+    public IrisBuilder withSepalLength(double sepalLength, double sepalWidth, double petalLength, double pet) {
+        this.sepalLength = sepalLength;
+        return this;
+    }
+
+    public IrisBuilder withSepalWidth(double sepalWidth, double sepalLength) {
+        this.sepalWidth = sepalWidth;
+        return this;
+    }
+
+    public IrisBuilder withPetalLength(double petalLength) {
+        this.petalLength = petalLength;
+        return this;
+    }
+
+    public IrisBuilder withPetalWidth(double petalWidth) {
+        this.petalWidth = petalWidth;
+        return this;
+    }
+
+    public IrisBuilder withVariety(String variety) {
+        this.variety = variety;
+        return this;
+    }
+
+    public Iris build() {
+        Iris iris = new Iris();
+        iris.setSepalLength(sepalLength);
+        iris.setSepalWidth(sepalWidth);
+        iris.setPetalLength(petalLength);
+        iris.setPetalWidth(petalWidth);
+        iris.setVariety(variety);
+        return iris;
+    }
+}
