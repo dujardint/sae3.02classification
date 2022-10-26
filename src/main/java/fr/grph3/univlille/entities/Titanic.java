9ad8f0634a8b344f4b103773335a734e002e16c7
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-10-26 11:42:10 +0200

    Update namings and complete Titanic builder
    - Public fields set to private

--- src/main/java/fr/grph3/univlille/entities/Titanic.java
+++ src/main/java/fr/grph3/univlille/entities/Titanic.java
@@ -4,174 +4,154 @@ import java.util.List;
 
 import com.opencsv.bean.CsvBindByName;
 
-public class Titanic implements Point{
-	
-	static List<Titanic> datas;
+public class Titanic implements Point {
 
-	@CsvBindByName(column = "PassengerId") 
-	public double passengerId;
-	@CsvBindByName(column = "Survived")
-	public double survived;
-	@CsvBindByName(column = "Pclass") 
-	public double pclass;
-	@CsvBindByName(column = "Name") 
-	public String name;
-	@CsvBindByName(column = "Sex") 
-	public String sex;
-	@CsvBindByName(column = "Age") 
-	public double age;
-	@CsvBindByName(column = "SibSp") 
-	public double sibSp;
-	@CsvBindByName(column = "Parch") 
-	public double parch;
-	@CsvBindByName(column = "Ticket") 
-	public String ticket;
-	@CsvBindByName(column = "Fare") 
-	public double fare;
-	@CsvBindByName(column = "Cabin") 
-	public String cabin;
-	@CsvBindByName(column = "Embarked") 
-	public String embarked;
-	
-	public Titanic() {
-	}
+    private List<Titanic> datas;
 
-	public Titanic(double passengerId, double survived, double pclass, String name, String sex, double age,
-			double sibSp, double parch, String ticket, double fare, String cabin, String embarked) {
-		this.passengerId = passengerId;
-		this.survived = survived;
-		this.pclass = pclass;
-		this.name = name;
-		this.sex = sex;
-		this.age = age;
-		this.sibSp = sibSp;
-		this.parch = parch;
-		this.ticket = ticket;
-		this.fare = fare;
-		this.cabin = cabin;
-		this.embarked = embarked;
-	}
+    @CsvBindByName(column = "PassengerId")
+    private double passengerId;
+    @CsvBindByName(column = "Survived")
+    private double survived;
+    @CsvBindByName(column = "Pclass")
+    private double pclass;
+    @CsvBindByName(column = "Name")
+    private String name;
+    @CsvBindByName(column = "Sex")
+    private String sex;
+    @CsvBindByName(column = "Age")
+    private double age;
+    @CsvBindByName(column = "SibSp")
+    private double sibSp;
+    @CsvBindByName(column = "Parch")
+    private double parch;
+    @CsvBindByName(column = "Ticket")
+    private String ticket;
+    @CsvBindByName(column = "Fare")
+    private double fare;
+    @CsvBindByName(column = "Cabin")
+    private String cabin;
+    @CsvBindByName(column = "Embarked")
+    private String embarked;
 
-	public double getPassengerId() {
-		return passengerId;
-	}
-	
-	public double getSurvived() {
-		return survived;
-	}
-	
-	public double getPclass() {
-		return pclass;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getSex() {
-		return sex;
-	}
-	
-	public double getAge() {
-		return age;
-	}
-	
-	public double getSibSp() {
-		return sibSp;
-	}
-	
-	public double getParch() {
-		return parch;
-	}
-	
-	public String getTicket() {
-		return ticket;
-	}
-	
-	public double getFare() {
-		return fare;
-	}
-	
-	public String getCabin() {
-		return cabin;
-	}
-	
-	public String getEmbarked() {
-		return embarked;
-	}
+    public double getPassengerId() {
+        return passengerId;
+    }
 
-	public void setPassenger_Id(double passenger_Id) {
-		this.passengerId = passenger_Id;
-	}
+    public double getSurvived() {
+        return survived;
+    }
 
-	public void setSurvived(double survived) {
-		this.survived = survived;
-	}
+    public double getPClass() {
+        return pclass;
+    }
 
-	public void setPclass(double pclass) {
-		this.pclass = pclass;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getSex() {
+        return sex;
+    }
 
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
+    public double getAge() {
+        return age;
+    }
 
-	public void setAge(double age) {
-		this.age = age;
-	}
+    public double getSibSp() {
+        return sibSp;
+    }
 
-	public void setSibSp(double sibSp) {
-		this.sibSp = sibSp;
-	}
+    public double getParch() {
+        return parch;
+    }
 
-	public void setParch(double parch) {
-		this.parch = parch;
-	}
+    public String getTicket() {
+        return ticket;
+    }
 
-	public void setTicket(String ticket) {
-		this.ticket = ticket;
-	}
+    public double getFare() {
+        return fare;
+    }
 
-	public void setFare(double fare) {
-		this.fare = fare;
-	}
+    public String getCabin() {
+        return cabin;
+    }
 
-	public void setCabin(String cabin) {
-		this.cabin = cabin;
-	}
+    public String getEmbarked() {
+        return embarked;
+    }
 
-	public void setEmbarked(String embarked) {
-		this.embarked = embarked;
-	}
-	
-	@Override
-	public Object getValue(Column<? extends Point> column) {
-		return column.getDataset();
-	}
+    public void setPassengerId(double passengerId) {
+        this.passengerId = passengerId;
+    }
 
-	@Override
-	public double getNormalizedValue(Column<? extends Point> xColumn) {
-		if(xColumn.isNormalizable()) {
-			return xColumn.getNormalizedValue(null);
-		}
-		return 0.0;
-	}
+    public void setSurvived(double survived) {
+        this.survived = survived;
+    }
 
-	@Override
-	public String toString() {
-		return "titanic [passengerId= " + passengerId + ", survived=" + survived + ", pclass=" + pclass + ", name="
-				+ name + ", sex=" + sex + ", age=" + age + ", sibSp=" + sibSp + ", parch=" + parch + ", ticket="
-				+ ticket + ", fare=" + fare + ", cabin=" + cabin + ", embarked=" + embarked + "]";
-	}
-	
-	
-	public List<Titanic> getClosest(List<Titanic> elements, int number) {
-	    // TODO Auto-generated method stub
-	    return null;
-	}
+    public void setPclass(double pClass) {
+        this.pclass = pClass;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public void setSibSp(double sibSp) {
+        this.sibSp = sibSp;
+    }
+
+    public void setParch(double parch) {
+        this.parch = parch;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public void setCabin(String cabin) {
+        this.cabin = cabin;
+    }
+
+    public void setEmbarked(String embarked) {
+        this.embarked = embarked;
+    }
+
+    @Override
+    public Object getValue(Column<? extends Point> column) {
+        return column.getDataset();
+    }
+
+    @Override
+    public double getNormalizedValue(Column<? extends Point> xColumn) {
+        if (xColumn.isNormalizable()) {
+            return xColumn.getNormalizedValue(null);
+        }
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "titanic [passengerId= " + passengerId + ", survived=" + survived + ", pclass=" + pclass + ", name="
+                + name + ", sex=" + sex + ", age=" + age + ", sibSp=" + sibSp + ", parch=" + parch + ", ticket="
+                + ticket + ", fare=" + fare + ", cabin=" + cabin + ", embarked=" + embarked + "]";
+    }
+
+
+    public List<Titanic> getClosest(List<Titanic> elements, int number) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
