8bfb6fd4c26c321d71b3f83cb19afd78501b17d8
Thomas Dujardin <thomas.dujardin2.etu@univ-lille.fr>
2022-11-21 10:58:47 +0100

    Tests points and columns

---
+++ src/test/java/fr/grph3/univlille/columns/TestNullColumn.java
@@ -0,0 +1,40 @@
+package fr.grph3.univlille.columns;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import fr.grph3.univlille.models.columns.NullColumn;
+
+
+public class TestNullColumn {
+	
+	NullColumn col = new NullColumn();
+	
+
+	@Test
+	public void test_get_normalized_value() {
+		assertEquals(0, col.getNormalizedValue(true));
+		assertEquals(0, col.getNormalizedValue(false));
+	}
+	
+	@Test
+	public void test_get_denormalized_value() {
+		assertEquals(null, col.getDenormalizedValue(0.0));
+		assertEquals(null, col.getDenormalizedValue(1.0));
+	}
+	
+	
+	@Test
+	public void test_get_name() {
+		assertEquals("Null Column", col.getName());
+	}
+	
+
+	@Test
+	public void test_isnormalisable() {
+		assertEquals(false, col.isNormalizable());
+	}
+
+}
