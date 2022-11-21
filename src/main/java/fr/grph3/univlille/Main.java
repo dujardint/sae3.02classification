8537f037748b87527c01c61433493fa5c589e145
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-11-21 10:45:41 +0100

    Fix conflicts

--- src/main/java/fr/grph3/univlille/Main.java
+++ src/main/java/fr/grph3/univlille/Main.java
@@ -1,8 +1,14 @@
 package fr.grph3.univlille;
 
+import fr.grph3.univlille.models.points.Iris;
+import fr.grph3.univlille.utils.CSVModel;
+
 public class Main {
 
     public static void main(String[] args) {
+        CSVModel<Iris> model = new CSVModel<>(Iris.class, "Iris");
+        model.loadFromFile("src/main/resources/iris.csv");
+        System.out.println(model.getColumns().toString());
     	System.out.println("Salut");
     }
 }
