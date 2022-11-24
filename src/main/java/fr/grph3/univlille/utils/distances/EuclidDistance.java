338a7a7b31e681bc47af5d3e295837791bd42cbe
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-11-24 11:28:52 +0100

    Refactor

--- src/main/java/fr/grph3/univlille/utils/distances/EuclidDistance.java
+++ src/main/java/fr/grph3/univlille/utils/distances/EuclidDistance.java
@@ -7,34 +7,24 @@ import java.util.List;
 
 public class EuclidDistance<T extends IPoint> implements IDistance<T> {
 
-    private final List<INormalizableColumn> columns;
+    private final List<IColumn> columns;
 
-    public EuclidDistance(List<INormalizableColumn> columns) {
+    public EuclidDistance(List<IColumn> columns) {
         this.columns = columns;
     }
     @Override
     public double distance(IPoint p1, IPoint p2) {
         if (p1 == null || p2 == null) return 0;
         double distance = 0.0;
-        for(INormalizableColumn column : columns ) {
-        	System.out.println(column.getName());
-            if(column instanceof NumberColumn)  {
-                double value1 = column.getNormalizedValue(p1.getValue(column));
-                double value2 = column.getNormalizedValue(p2.getValue(column));
+        for(IColumn column : columns ) {
+            if(column instanceof INormalizableColumn) {
+                double value1 = ((INormalizableColumn) column).getNormalizedValue(p1.getValue(column));
+                double value2 = ((INormalizableColumn) column).getNormalizedValue(p2.getValue(column));
                 distance += Math.pow(value1 - value2, 2);
+            } else {
+                distance += p1.getValue(column).equals(p2.getValue(column)) ? 0 : 1;
+            }
 
-            }
-            if (column instanceof BooleanColumn) {
-                double value1 = column.getNormalizedValue(p1.getValue(column));
-                double value2 = column.getNormalizedValue(p2.getValue(column));
-                distance += Math.pow(value1 - value2, 2);
-
-            }
-            if (column instanceof StringColumn) {
-                String value1 = (String) p1.getValue(column);
-                String value2 = (String) p2.getValue(column);
-                distance += value1.equals(value2) ? 0 : 1;
-            }
         }
         return Math.sqrt(distance);
     }
