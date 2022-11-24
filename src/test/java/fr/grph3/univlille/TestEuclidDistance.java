fa9552a608aae7370a25ded5e2624e992bfa1fde
Nathan Accart <nathan.accart.etu@univ-lille.fr>
2022-11-24 10:08:57 +0100

    Add a CSVtest, EuclidDistanceTest and BooleanColumnTEst

---
+++ src/test/java/fr/grph3/univlille/TestEuclidDistance.java
@@ -0,0 +1,30 @@
+package fr.grph3.univlille;
+
+import fr.grph3.univlille.models.points.Titanic;
+import fr.grph3.univlille.utils.CSVModel;
+import fr.grph3.univlille.utils.distances.EuclidDistance;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEuclidDistance {
+
+    private CSVModel<Titanic> model;
+
+    private EuclidDistance<Titanic> distance;
+
+    @BeforeEach
+    void setUp(){
+        this.model = new CSVModel<>(Titanic.class, "Titanic");
+        model.loadFromFile("src/main/resources/titanicTest.csv");
+        this.distance = new EuclidDistance<>(model.getNormalizableColumns());
+    }
+
+    @Test
+    void testDistance(){
+        assertEquals(0.89, Math.round (distance.distance(model.getPoint(2), model.getPoint(3) ) *100.0)/100.0);
+        assertEquals(1.30, Math.round (distance.distance(model.getPoint(0), model.getPoint(2) ) *100.0)/100.0);
+        assertEquals(1.94,Math.round (distance.distance(model.getPoint(1), model.getPoint(3) ) *100.0)/100.0);
+    }
+}
