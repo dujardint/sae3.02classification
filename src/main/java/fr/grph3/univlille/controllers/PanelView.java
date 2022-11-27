92d0b5eabfb4d24e62b3c14b9d39844162edb2ca
Thomas Dujardin <thomas.dujardin2.etu@univ-lille.fr>
2022-11-27 16:49:14 +0100

    SetTitle("Classificator") on panelView

--- src/main/java/fr/grph3/univlille/controllers/PanelView.java
+++ src/main/java/fr/grph3/univlille/controllers/PanelView.java
@@ -74,6 +74,7 @@ public class PanelView extends AbstractView {
 
     public PanelView(Stage stage) {
         super(stage);
+        stage.setTitle("Classificator");
         this.modelManager = new MVCModelManager();
     }
 
