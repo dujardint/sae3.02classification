02112982c0a832a3827fbccb26af3fdffef43b58
Ehis Okpebho <ehis.okpebho.etu@univ-lille.fr>
2022-12-01 00:12:13 +0100

    Delete unused class

--- src/main/java/fr/grph3/univlille/utils/ResourceUtil.java
+++
@@ -1,8 +0,0 @@
-package fr.grph3.univlille.utils;
-
-public class ResourceUtil {
-
-    public static String get(String path) {
-        return ResourceUtil.class.getResource(path).toExternalForm();
-    }
-}
